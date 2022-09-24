9d92229acf61f2774800cdbe9cbed8cc10337b13
mathurpulkit <pmathur2001@gmail.com>
2022-09-24 15:30:35 +0530

    MST seems to be working fine now, some testing still left.

--- mst.cpp
+++ mst.cpp
@@ -1,52 +1,56 @@
 #include"mst.h"
-
+#include<set>
 using namespace std;
 
 int prim(vector<vector<int>> graph, int startingPoint) {
-    // initialize the open list
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> openList;
-    // initialize the closed list
-    vector<bool> closedList(graph.size(), false);
-    // initialize the path
-    vector<int> path;
-    // add the starting point to the open list
-    openList.push(make_pair(0, startingPoint));
-    // while the open list is not empty
-    while (!openList.empty()) {
-        // get the node with the lowest f value from the open list
-        int currentNode = openList.top().second;
-        openList.pop();
-        // add the node to the closed list
-        closedList[currentNode] = true;
-        // if the node is the goal node
-        if (path.size() == graph.size()) {
-            // add the node to the path
-            path.push_back(currentNode);
-            // return the path cost
-            //return path;
-            int ans=0;
-            for(int i = 0; i < path.size(); i++) {
-                ans += path[i];
-            }
-            return ans;
-        }
-        // for each neighbor of the node
+    // use prim's algorithm to find the minimum spanning tree
+    // initialize the minimum spanning tree
+    vector<vector<int>> mst(graph.size(), vector<int>(graph.size(), 0));
+    // initialize the set of visited nodes
+    set<int> visited;
+    // add the starting point to the set of visited nodes
+    visited.insert(startingPoint);
+    // while the set of visited nodes is not equal to the set of all nodes
+    while(visited.size()<graph.size()){
+        // initialize the minimum edge
+        int minEdge = numeric_limits<int>::max();
+        // initialize the minimum edge's starting point
+        int minEdgeStartingPoint = 0;
+        // initialize the minimum edge's ending point
+        int minEdgeEndingPoint = 0;
+        // for each node in the set of visited nodes
         for (int i = 0; i < graph.size(); i++) {
-            // if the neighbor is not in the closed list
-            if (!closedList[i]) {
-                // if the neighbor is not in the open list
-                if (graph[currentNode][i] != 0 && graph[currentNode][i] != numeric_limits<int>::max()) {
-                    // add the neighbor to the open list
-                    openList.push(make_pair(graph[currentNode][i], i));
+            if (visited.find(i) != visited.end()) {
+                // for each node in the graph
+                for (int j = 0; j < graph.size(); j++) {
+                    // if the node is not in the set of visited nodes
+                    if (visited.find(j) == visited.end()) {
+                        // if the edge is smaller than the minimum edge
+                        if (graph[i][j] < minEdge) {
+                            // update the minimum edge
+                            minEdge = graph[i][j];
+                            // update the minimum edge's starting point
+                            minEdgeStartingPoint = i;
+                            // update the minimum edge's ending point
+                            minEdgeEndingPoint = j;
+                        }
+                    }
                 }
             }
         }
+        // add the minimum edge to the minimum spanning tree
+        mst[minEdgeStartingPoint][minEdgeEndingPoint] = minEdge;
+        mst[minEdgeEndingPoint][minEdgeStartingPoint] = minEdge;
+        // add the minimum edge's ending point to the set of visited nodes
+        visited.insert(minEdgeEndingPoint);
     }
-    // return the path length
-    //return path;
-    int ans=0;
-    for(int i=0;i<path.size();i++){
-        ans+=path[i];
+    // calculate the total weight of the minimum spanning tree
+    int totalWeight = 0;
+    for (int i = 0; i < mst.size(); i++) {
+        for (int j = 0; j <= i; j++) {
+            totalWeight += mst[i][j];
+        }
     }
-    return ans;
+    // return the total weight of the minimum spanning tree
+    return totalWeight;
 }
