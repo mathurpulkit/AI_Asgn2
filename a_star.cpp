0a8b03028c6c29db4ada3ac4e928544623759c4e
mathurpulkit <pmathur2001@gmail.com>
2022-09-24 17:12:01 +0530

    A_start still not working prob because of priority queue improper implementation

--- a_star.cpp
+++ a_star.cpp
@@ -3,72 +3,57 @@
 using namespace std;
 
 vector<int> a_star(vector<vector<int>> graph, int startingPoint) {
-    // use prim as the heuristic function
-    // use prim to get the MST of the subgraph of all the nodes not visited yet, and the starting point
-    // initialize the open list
-    set <pair<int, int>> openList;
-    // initialize the closed list
-    set <int> closedList;
-    // initialize the path
+    // implement the A* algorithm on the graph to solve the traveling salesman problem
+    // heuristic function is using prim on the nodes that are not visited, and the startingPoint
+    // return the path that the salesman should take
+    vector<int> ans;
+    // create the vector to track the path, it gets continuously modified as the algorithm progresses
     vector<int> path;
-    // add the starting point to the open list
-    openList.insert(make_pair(0, startingPoint));
-    // while the open list is not empty
-    while (!openList.empty()) {
-        // get the node with the lowest f value from the open list
-        int currentNode = openList.begin()->second;
-        // add the node to the closed list
-        closedList.insert(currentNode);
-        // remove the node from the open list
-        openList.erase(openList.begin()); // GIVES WRONG ANSWER, BUT THIS IS PROBABLY RIGHT
-        if (closedList.size() == graph.size()) {
-            // add the node to the path
-            path.push_back(currentNode);
+    path.push_back(startingPoint);
+    // create the expanded list to track the nodes that have been expanded
+    set<vector<int>> expanded; // expanded[i] stores the path that has been expanded, eg: 1,2,5,4
+    // create the priority queue to store the nodes that are to be expanded, it should store path, judge by cost of path
+    
+    // loop till the path is not complete or the priority queue is not empty
+    while(path.size() != graph.size() && !pq.empty()) {
+        // get the path from the top of the priority queue
+        vector<int> temp = pq.top();
+        pq.pop();
+        // check if the path has already been expanded
+        if (expanded.find(temp) != expanded.end()) {
+            continue;
         }
-        // for each neighbor of the node
+        // add the path to the expanded list
+        expanded.insert(temp);
+        // get the last node in the path
+        int lastNode = temp[temp.size() - 1];
+        // loop through all the nodes in the graph
         for (int i = 0; i < graph.size(); i++) {
-            // if the neighbor is not in the closed list
-            if (closedList.find(i) == closedList.end()) {
-                // if the neighbor is not in the open list
-                if (graph[currentNode][i] != 0 && graph[currentNode][i] != numeric_limits<int>::max()) {
-                    // add the neighbor to the open list
-                    // compute the heuristic value
-                    // get the subgraph of all the nodes not visited yet, and the starting point
-                    vector<vector<int>> subGraph(graph.size(), vector<int>(graph.size(), 0));
-                    for (int j = 0; j < graph.size(); j++) {
-                        for (int k = 0; k < graph.size(); k++) {
-                            if (closedList.find(j) == closedList.end() && closedList.find(k) == closedList.end()) {
-                                subGraph[j][k] = graph[j][k];
-                            }
-                        }
-                    }
-                    // starting point for the prim is the neighbor
-                    openList.insert(make_pair(graph[currentNode][i] + prim(subGraph, i), i));
-                }
-                // if the neighbor is in the open list
-                else {
-                    // if the neighbor's g value is greater than the current g value
-                    // compute the heuristic value
-                    // get the subgraph of all the nodes not visited yet, and the starting point
-                    vector<vector<int>> subGraph(graph.size(), vector<int>(graph.size(), 0));
-                    for (int j = 0; j < graph.size(); j++) {
-                        for (int k = 0; k < graph.size(); k++) {
-                            if (closedList.find(j) == closedList.end() && closedList.find(k) == closedList.end()) {
-                                subGraph[j][k] = graph[j][k];
-                            }
-                        }
-                    }
-                    if (graph[currentNode][i] + prim(subGraph, i) < graph[currentNode][i]) {
-                        // update the neighbor's g value
-                        graph[currentNode][i] = graph[currentNode][i] + prim(subGraph, i);
-                        // update the neighbor's parent
-                        graph[i][currentNode] = currentNode;
-                    }
+            // check if the node is already in the path
+            if (find(temp.begin(), temp.end(), i) != temp.end()) {
+                continue;
+            }
+            // create a new path by adding the node to the current path
+            vector<int> newPath = temp;
+            newPath.push_back(i);
+            // calculate the cost of the path
+            int cost = 0;
+            for (int j = 0; j < newPath.size() - 1; j++) {
+                cost += graph[newPath[j]][newPath[j + 1]];
+            }
+            // add the heuristic cost to the cost
+            // create the subgraph of unexpanded nodes
+            vector<vector<int>> subgraph;
+            for (int j = 0; j < graph.size(); j++) {
+                if (find(newPath.begin(), newPath.end(), j) == newPath.end()) {
+                    subgraph.push_back(graph[j]);
                 }
             }
+            // add the mst cost to the cost
+            cost += prim(subgraph, i);
+            // add the path and cost to the priority queue
+            pq.push({cost, newPath});
         }
     }
-    // return the path
-    // convert set to vector for returning
-    return path;
+    return ans;
 }
