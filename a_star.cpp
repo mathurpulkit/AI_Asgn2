4887150008e71399a8d92f0e91ce33e0e4210f27
mathurpulkit <pmathur2001@gmail.com>
2022-09-24 19:15:26 +0530

    Still not working, but might work with minor changes

--- a_star.cpp
+++ a_star.cpp
@@ -2,58 +2,72 @@
 
 using namespace std;
 
+typedef pair<int, vector<int>> pq_pair;
+
+// implement A* search algorithm, return the path
+// use prim function from mst.h as heuristic
 vector<int> a_star(vector<vector<int>> graph, int startingPoint) {
-    // implement the A* algorithm on the graph to solve the traveling salesman problem
-    // heuristic function is using prim on the nodes that are not visited, and the startingPoint
-    // return the path that the salesman should take
-    vector<int> ans;
-    // create the vector to track the path, it gets continuously modified as the algorithm progresses
-    vector<int> path;
-    path.push_back(startingPoint);
-    // create the expanded list to track the nodes that have been expanded
-    set<vector<int>> expanded; // expanded[i] stores the path that has been expanded, eg: 1,2,5,4
-    // create the priority queue to store the nodes that are to be expanded, it should store path, judge by cost of path
-    
-    // loop till the path is not complete or the priority queue is not empty
-    while(path.size() != graph.size() && !pq.empty()) {
-        // get the path from the top of the priority queue
-        vector<int> temp = pq.top();
-        pq.pop();
-        // check if the path has already been expanded
-        if (expanded.find(temp) != expanded.end()) {
-            continue;
-        }
-        // add the path to the expanded list
-        expanded.insert(temp);
-        // get the last node in the path
-        int lastNode = temp[temp.size() - 1];
-        // loop through all the nodes in the graph
-        for (int i = 0; i < graph.size(); i++) {
-            // check if the node is already in the path
-            if (find(temp.begin(), temp.end(), i) != temp.end()) {
-                continue;
+    // create a priority queue, which is a min heap, represents the fringe list
+    priority_queue<pq_pair, vector<pq_pair>, greater<pq_pair>> fringe;
+    // create a set to store the visited paths
+    set<vector<int>> visited;
+    // initialise fringe list
+    fringe.push(make_pair(0, vector<int>(startingPoint)));
+    // loop until fringe list is empty
+    while(!fringe.empty()){
+        // get the path with the lowest cost
+        vector<int> path = fringe.top().second;
+        // check if the path is already visited
+        if (visited.find(path) == visited.end()) {
+            // if not visited, add the path to the visited set
+            visited.insert(path);
+            // check if the path is a complete path
+            if (path.size() == graph.size()) {
+                // if the path is complete, return the path
+                return path;
             }
-            // create a new path by adding the node to the current path
-            vector<int> newPath = temp;
-            newPath.push_back(i);
-            // calculate the cost of the path
-            int cost = 0;
-            for (int j = 0; j < newPath.size() - 1; j++) {
-                cost += graph[newPath[j]][newPath[j + 1]];
-            }
-            // add the heuristic cost to the cost
-            // create the subgraph of unexpanded nodes
-            vector<vector<int>> subgraph;
-            for (int j = 0; j < graph.size(); j++) {
-                if (find(newPath.begin(), newPath.end(), j) == newPath.end()) {
-                    subgraph.push_back(graph[j]);
+            // if the path is not complete, expand the path
+            else {
+                // get the last node in the path
+                int lastNode = path.back();
+                // loop through the graph
+                for (int i = 0; i < graph.size(); i++) {
+                    // if i in path, continue
+                    if (find(path.begin(), path.end(), i) != path.end()) {
+                        continue;
+                    }
+                    // check if the node is connected to the last node in the path
+                    if (graph[lastNode][i] != MAXINT) {
+                        // if connected, create a new path
+                        vector<int> newPath = path;
+                        // add the new node to the new path
+                        newPath.push_back(i);
+                        // calculate the cost of the new path
+                        int cost = fringe.top().first + graph[lastNode][i];
+                        // create a subgraph for the unvisited nodes
+                        vector<int> unvisitedNodes;
+                        unvisitedNodes.push_back(i);
+                        for (int j = 0; j < graph.size(); j++) {
+                            if (find(path.begin(), path.end(), j) == path.end()) {
+                                unvisitedNodes.push_back(j);
+                            }
+                        }
+                        vector<vector<int>> subgraph;
+                        for (int j = 0; j < unvisitedNodes.size(); j++) {
+                            subgraph.insert(subgraph.begin() + j, vector<int>());
+                            for (int k = 0; k < unvisitedNodes.size(); k++) {
+                                subgraph[j].insert(subgraph[j].begin() + k, graph[unvisitedNodes[j]][unvisitedNodes[k]]);
+                            }
+                        }
+                        cost += prim(subgraph, 0);
+                        // add the new path to the fringe list
+                        fringe.push(make_pair(cost, newPath));
+                    }
                 }
             }
-            // add the mst cost to the cost
-            cost += prim(subgraph, i);
-            // add the path and cost to the priority queue
-            pq.push({cost, newPath});
         }
+        // remove the path with the lowest cost from the fringe list
+        fringe.pop();
     }
-    return ans;
+    return vector<int>();
 }
